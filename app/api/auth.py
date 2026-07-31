f7ee02ffbb7ea660bc55e40890d92c4dfbd8cd61
Prabhaker Singh <iprabhakersingh@gmail.com>
2026-07-31 10:08:55 +0530

    Initial project setup

---
+++ app/api/auth.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.db.database import get_db
+from app.models.user import User
+from app.schemas.user import UserRegister, UserResponse
+from app.core.security import hash_password
+
+router = APIRouter(prefix="/auth", tags=["Authentication"])
+
+
+@router.post("/signup", response_model=UserResponse)
+def signup(user: UserRegister, db: Session = Depends(get_db)):
+
+    existing_user = db.query(User).filter(User.email == user.email).first()
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        password=hash_password(user.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+from app.db.database import Base, engine
+from app.models.user import User
+from app.api.auth import router as auth_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="TaskFlow API")
+
+app.include_router(auth_router)
+
+
+@app.get("/")
+def root():
+    return {"message": "TaskFlow API is running"}
