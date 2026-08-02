f32f625428bd7fb2e251aabf9be4d01eaa2bffc3
Prabhaker Singh <iprabhakersingh@gmail.com>
2026-08-02 19:03:04 +0530

    Support configurable Redis connection for Deployment

--- app/core/celery_app.py
+++ app/core/celery_app.py
@@ -1,10 +1,11 @@
 from celery import Celery
 from celery.schedules import crontab
+import os
 
 celery_app = Celery(
     "taskflow",
-    broker="redis://redis:6379/0",
-    backend="redis://redis:6379/0",
+    broker=os.getenv("REDIS_URL", "redis://redis:6379/0"),
+    backend=os.getenv("REDIS_URL", "redis://redis:6379/0"),
 )
 
 celery_app.conf.update(
